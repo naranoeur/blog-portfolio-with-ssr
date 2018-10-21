97a72abb4fe1fa2ec47c3dde46842e170803f103
naranoeur <naranoeur@gmail.com>
2018-10-21 18:23:57 +0900

    gogole an

---
+++ client/serverRenderers/photos/googleAnalytics.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+  return `
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111588482-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-111588482-3');
+    </script>
+  `;
+}
